cd219a76ad9c89f7380a63c79711ed1a9a67d742
ZeraiGR <kirillgr15@mail.ru>
2025-03-09 21:29:26 +0300

    fix syntax error

--- sql/migrations/V4__alter_users_and_posts_table.sql
+++ sql/migrations/V4__alter_users_and_posts_table.sql
@@ -1,15 +1,8 @@
-WITH inserted_users AS (
-    INSERT INTO users (name, email) VALUES 
-    ('Иван Иванов', 'ivan.ivanov@example.com'),
-    ('Анна Смирнова', 'anna.smirnova@example.com')
-    RETURNING id, name
-);
-
-INSERT INTO posts (text, owner_id) 
-SELECT 
-    CASE 
-        WHEN name = 'Иван Иванов' THEN 'Первый пост от Ивана Иванова'
-        WHEN name = 'Анна Смирнова' THEN 'Первый пост от Анны Смирновой'
-    END,
-    id
-FROM inserted_users;
+WITH u AS (
+  INSERT INTO users (name)
+  VALUES ('Alice'), ('Bob')
+  RETURNING id
+)
+INSERT INTO posts (text, owner_id)
+SELECT CONCAT('My ', u.id, ' post'), u.id
+FROM u; 
